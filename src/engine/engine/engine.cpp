7b84d90e71ada455ea02c5bf489ce456e0295feb
obfuscate <obfuscate-source@yandex.ru>
2025-02-27 17:34:27 +0200

    Add a new 3rd party: ufbx.
    Thanks a lot to Petr!

--- src/engine/engine/engine.cpp
+++ src/engine/engine/engine.cpp
@@ -1,5 +1,7 @@
 #include <engine/engine.h>
 
+#include <ufbx/ufbx.h>
+
 #include <iostream>
 
 namespace engine
@@ -7,6 +9,10 @@ namespace engine
 
 bool Engine::initialize()
 {
+	ufbx_load_opts opts = { 0 };
+	ufbx_error error;
+	[[maybe_unused]] ufbx_scene* scene = ufbx_load_file("thing.fbx", &opts, &error);
+
 	std::cout << "Initialize" << std::endl;
 	return true;
 }
